ed5beead6929344494428b2514373c0bbede2eef
Wyattwicks <wyattwicks@gmail.com>
2021-06-02 18:48:33 -0600

    Add basic testing

--- spec/users/dashboard_spec.rb
+++ spec/users/dashboard_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 RSpec.describe "User Dashboard Page" do
   before :each do
   end
+  
+  skip "has a button to log-out" do
+    expect(page).to have_button("log Out")
+  end
+
+  skip "has a button to search" do
+    expect(page).to have_button("Search Elections")
+  end
+
+  skip "has elections I followed" do 
+
+  end
 end
--- spec/users/welcome_spec.rb
+++ spec/users/welcome_spec.rb
@@ -3,4 +3,8 @@ RSpec.describe "Welcome Page" do
   before :each do
     visit root_path
   end
+
+  skip "has a welcome message" do
+    expect(page).to have_content("Welcome to Vote Local")
+  end
 end
