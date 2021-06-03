c1162b49717c2d6619058c728f75c1543f59c896
Harrison Blake <harrison.r.blake.gmail.com>
2021-06-03 13:30:40 -0600

    Start welcome page test

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+def WelcomeController < ApplicationController
+  def index
+
+  end
+end
--- spec/users/welcome_spec.rb
+++ spec/users/welcome_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
+
 RSpec.describe "Welcome Page" do
   before :each do
     visit root_path
+  end
+
+  describe "When we visit the welcome page" do
+    it "displays our projects title and a button to log into the site with twitter" do
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_content("Vote Local")
+      expect(page).to have_button("Login with Twitter")
+    end
   end
 end
