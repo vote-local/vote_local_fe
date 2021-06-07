82754749a7b5dd450b47f04b3e4e80f801bd2589
Andrew Shafer <73140608+Aphilosopher30@users.noreply.github.com>
2021-06-06 18:13:31 -0600

    fix test for search feature

--- spec/search/search_spec.rb
+++ spec/search/search_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe 'User search Page' do
       @user = User.create!(uid: '23782', username: 'testuser', token: 'kjh76sd876sd')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
+      response = File.read("./spec/fixtures/representatives_index.json")
+
+      stub_request(:get, "https://vote-local-be.herokuapp.com/api/v1/representatives?address=901%20N%20Sherman%20street,%20Denver,%20CO,%2080203").
+               with(
+                 headers: {
+             	  'Accept'=>'*/*',
+             	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+             	  'User-Agent'=>'Faraday v1.4.2'
+                 }).
+               to_return(status: 200, body: response, headers: {})
+
+
       visit '/search'
 
       fill_in 'zip_code', with: '80203'
@@ -34,6 +46,8 @@ RSpec.describe 'User search Page' do
       fill_in 'street_adress', with: '901 N Sherman street'
 
       click_button 'search'
+
+
 
       expect(current_path).to eq('/representatives')
     end
