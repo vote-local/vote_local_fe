e7cd9e16477598bd78720072d39d333130ef0632
Harrison Blake <harrison.r.blake.gmail.com>
2021-06-08 16:07:32 -0600

    Add twitter_rep model

---
+++ app/models/twitter_rep.rb
@@ -0,0 +1,3 @@
+class TwitterRep < ApplicationRecord
+  belongs_to :user
+end
---
+++ db/migrate/20210608215845_create_twitter_reps.rb
@@ -0,0 +1,12 @@
+class CreateTwitterReps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :twitter_reps do |t|
+      t.string :name
+      t.string :api_id
+      t.string :twitter
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
