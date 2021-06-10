a6f3e2169c7f2a11009d5519ad862c79a84d0e94
koshea9 <keegan.oshea9@gmail.com>
2021-06-09 20:53:48 -0600

    tried various oauth headers

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -11,18 +11,30 @@ class TwitterService
     id = result[:data][:id]
   end
 
-  # def self.follow(twitter_id, user_id)
-  #   # binding.pry
-  #   response = conn.post("users/#{user_id}/following") do |require|
-  #     # require.params['id'] = user_id
-  #     require.headers['Consumer Key'] = "#{ENV['api_key']}"
-  #     require.headers['Consumer Secret'] = "#{ENV['api_secret']}"
-  #     require.headers['Access Token'] = "#{ENV['twitter_access_token']}"
-  #     require.headers['Token Secret'] = "#{ENV['twitter_access_secret_token']}"
-  #     require.body = "{target_user_id: #{twitter_id}}"
-  #   end
-  #   # JSON.parse(response.body, symbolize_names: true)
-  # end
+  def self.follow(twitter_id, user_id)
+    # binding.pry
+    response = conn.post("users/#{user_id}/following") do |require|
+      # require.params['id'] = user_id
+      require.headers['content_type'] = 'application/json'
+      require.headers['oauth_consumer_key'] = "#{ENV['api_key']}"
+      require.headers['consumer_secret'] = "#{ENV['api_secret']}"
+      require.headers['oauth_token'] = "#{ENV['twitter_access_token']}"
+      require.headers['oauth_token_secret'] = "#{ENV['twitter_access_secret_token']}"
+      require.headers['oauth_version'] = "1.0"
+      require.headers['oauth_signature_method'] = "HMAC-SHA1"
+      require.body = "{target_user_id: #{twitter_id}}"
+    end
+
+    # JSON.parse(response.body, symbolize_names: true)
+  end
+
+# oauth_consumer_key="z8C8OPOi1sRHGQk6SqwmaC9cw",
+  # oauth_token="1399770258081128450-qm2TDGXNmMsYiW7MUBcr6WOpgLGXii",
+  # oauth_signature_method="HMAC-SHA1",
+  # oauth_timestamp="1623289899",
+  # oauth_nonce="Oopo35Z5okw",
+  # oauth_version="1.0",
+  # oauth_signature="Y6Go080n%2Bxk%2FApzmBNVQaeuDicE%3D"
 
   # def self.find_tweets(rep_name)
   #   response = conn.get("tweets/search/recent") do |req|
