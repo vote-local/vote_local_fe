4a6b4bf7183e37e2610cf45d71c1fc396f21b848
Harrison Blake <harrison.r.blake.gmail.com>
2021-06-09 18:12:19 -0600

    Displays tweets on page needs refactor and tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,11 @@ class DashboardController < ApplicationController
   before_action :require_current_user
 
   def show
+    if !current_user.twitter_reps.empty?
+      twt_handle = current_user.twitter_reps.first.twitter
+      twitter_id = TwitterService.representative_twitter_id(twt_handle)
+      @representative_tweets = TwitterService.rep_tweets(twitter_id)
+    end
   end
 
   def create
